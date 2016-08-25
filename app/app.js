658c77c010ab0d4ad547b263d597aa6c944e3e56
stevenliao <Stvnliao@yahoo.com>
2016-08-24 21:29:58 -0400

    Added chat

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 var data = require('./data/data.json');
+var io = require('socket.io')();
 
 // Set variables for the express app
 app.set('port', process.env.PORT || 3000 );
@@ -18,10 +19,28 @@ app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/animes'));
 app.use(require('./routes/api'));
+app.use(require('./routes/chat'));
 
 var server = app.listen(app.get('port'), function()
 {
     console.log('Listening on port ' + app.get('port'));
+});
+
+// Attach io to the server and start listening for connected users chat messages
+io.attach(server);
+io.on('connection', function(socket)
+{
+    console.log('User connected');
+    
+    socket.on('postMessage', function(data)
+    {
+        io.emit('updateMessages', data);
+    });
+    
+    socket.on('disconnect', function()
+    {
+        console.log('User disconnected');
+    });
 });
 
 reload(server, app);
