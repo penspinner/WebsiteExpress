5311438009df3585ec42c40c2f1cdbb1faef0c11
root <root@stevenliao.tech>
2016-11-12 17:04:43 -0500

    Fixed merge

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,11 +4,7 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var animeData = require('../data/animes.json');
 
-<<<<<<< HEAD
 router.get(['/api', '/nodejsAnime/api'], function(req, res)
-=======
-router.get('/api', function(req, res)
->>>>>>> ff4e5ac19c8e5505868c72e3555a08bf5b7b542d
 {
     res.json(animeData);
 });
@@ -16,11 +12,7 @@ router.get('/api', function(req, res)
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
-router.post(['/api', '/nodejsAnime/api'], function(req, res) 
-=======
-router.post('/api', function(req, res) 
->>>>>>> ff4e5ac19c8e5505868c72e3555a08bf5b7b542d
+router.post(['/api', '/nodejsAnime/api'], function(req, res)
 {
     animeData.unshift(req.body);
     fs.writeFile('app/data/animes.json', JSON.stringify(animeData), 'utf8', function(err) 
@@ -31,12 +23,7 @@ router.post('/api', function(req, res)
     res.json(animeData);
 });
 
-
-<<<<<<< HEAD
 router.delete(['/api/:id', '/nodejsAnime/api/:id'], function(req, res) 
-=======
-router.delete('/api/:id', function(req, res) 
->>>>>>> ff4e5ac19c8e5505868c72e3555a08bf5b7b542d
 {
     animeData.splice(req.params.id, 1);
     fs.writeFile('app/data/animes.json', JSON.stringify(animeData), 'utf8', function(err) 
